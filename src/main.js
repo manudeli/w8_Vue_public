6d3b7b57697d5ddaa94730bd6e6cef428924504e
ksh <ksahoo524@gmail.com>
2021-10-06 16:37:33 +0900

    initial commit

---
+++ src/main.js
@@ -0,0 +1,9 @@
+import { createApp } from 'vue';
+import App from '~/App';
+import store from '~/store';
+import router from '~/routes';
+
+const app = createApp(App);
+app.use(router);
+app.use(store);
+app.mount('#app');
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+const path = require('path');
+const { VueLoaderPlugin } = require('vue-loader');
+const HtmlPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  resolve: {
+    extensions: ['.vue', '.js'],
+    alias: {
+      '~': path.resolve(__dirname, 'src')
+    }
+  },
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+    publicPath: '/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        use: 'vue-loader'
+      },
+      {
+        test: /\.s?css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader',
+          'sass-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+                additionalData: '@use "sass:color"; @import "~/scss/_variables";'
+            }
+        }
+      ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin(),
+    new HtmlPlugin({
+      template: './src/index.html'
+    }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'static' }
+      ]
+    })
+  ],
+  devServer: {
+    port: 8080,
+    historyApiFallback: true
+  }
+};
